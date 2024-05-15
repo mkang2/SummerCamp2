0510dfb2372093ccdba93eb25a6cff0732ae3a66
mkang2 <kangmichaelj1@gmail.com>
2024-05-15 19:51:33 -0400

    add google maps

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -3,7 +3,16 @@ import "./ContactUs.css";
 const ContactUs = () => {
   return (
     <div className="frame-parent15">
-      <img className="frame-child3" loading="lazy" alt="" src="/Suitcase.png" />
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3037.5331370115346!2d-74.4213562883733!3d40.419192171320105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3c577e0c24179%3A0x29995c444322d64f!2s572%20Ryders%20Ln%2C%20East%20Brunswick%2C%20NJ%2008816!5e0!3m2!1sen!2sus!4v1715816873808!5m2!1sen!2sus"
+        width="500"
+        height="350"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+      {/* <img className="frame-child3" loading="lazy" alt="" src="/Suitcase.png" /> */}
       <i className="address-572-ryders">
         Address: 572 Ryders Lane, East Brunswick, NJ 08816
       </i>
