f6857f28799c4ab7d78dab33ef780a27a68d87ed
Robert Kang <65458638+rokanq@users.noreply.github.com>
2024-05-23 17:53:08 -0400

    home page done ish

--- src/home.js
+++ src/home.js
@@ -1,13 +1,74 @@
 import React from 'react';
 
 function HomePage() {
-  return (
-    <div>
-      <img src="\Frame3.png" alt="summer camp photo"></img>
-      <img src="\frame-4@2x.png" alt="kid painting"></img>
+    return (
+        <div>
+            <img src="\Frame3.png" alt="summer camp photo"></img>
+            <div className="bg-white py-6 px-4 md:px-40 m-4 grid md:grid-cols-2 md:gap-x-12 items-center">
+                <img src="/frame-4@2x.png" alt="Students engaged in activity" className="rounded-lg mx-auto md:order-2" style={{ maxWidth: '100%' }} />
+                <div>
+                    <div className="text-center">
+                        <h3 className="text-2xl font-bold mt-3 mb-4">Our Vision</h3>
+                        <p className="text-gray-700 mb-4">
+                            Our vision is to provide a space for students to be academically
+                            and socially engaged in a fun and friendly environment. Our goal is
+                            to empower each child with the tools and resources they need to
+                            navigate the challenges of the upcoming school year with
+                            resilience and enthusiasm!
+                        </p>
+                    </div>
+                    <div className="flex justify-center">
+                        <button className="bg-yellow-400 text-white rounded-lg px-4 py-2 hover:bg-yellow-300">
+                            Learn More
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-white py-6 px-4 md:px-40 m-4 grid md:grid-cols-2 md:gap-x-12 items-center">
+                <img src="/frame-4@2x.png" alt="Students engaged in activity" className="rounded-lg mx-auto" style={{ maxWidth: '100%' }} />
+                <div>
+                    <div className="text-center">
+                        <h3 className="text-2xl font-bold mt-3 mb-4">Our Vision</h3>
+                        <p className="text-gray-700 mb-4">
+                            Our vision is to provide a space for students to be academically
+                            and socially engaged in a fun and friendly environment. Our goal is
+                            to empower each child with the tools and resources they need to
+                            navigate the challenges of the upcoming school year with
+                            resilience and enthusiasm!
+                        </p>
+                    </div>
+                    <div className="flex justify-center">
+                        <button className="bg-yellow-400 text-white rounded-lg px-4 py-2 hover:bg-yellow-300">
+                            Learn More
+                        </button>
+                    </div>
+                </div>
 
-    </div>
-  );
+            </div>
+            <div className="bg-white py-6 px-4 md:px-40 m-4 grid md:grid-cols-2 md:gap-x-12 items-center">
+                <img src="/frame-4@2x.png" alt="Students engaged in activity" className="rounded-lg mx-auto md:order-2" style={{ maxWidth: '100%' }} />
+                <div>
+                    <div className="text-center">
+                        <h3 className="text-2xl font-bold mt-3 mb-4">Our Vision</h3>
+                        <p className="text-gray-700 mb-4">
+                            Our vision is to provide a space for students to be academically
+                            and socially engaged in a fun and friendly environment. Our goal is
+                            to empower each child with the tools and resources they need to
+                            navigate the challenges of the upcoming school year with
+                            resilience and enthusiasm!
+                        </p>
+                    </div>
+                    <div className="flex justify-center">
+                        <button className="bg-yellow-400 text-white rounded-lg px-4 py-2 hover:bg-yellow-300">
+                            Learn More
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default HomePage;
