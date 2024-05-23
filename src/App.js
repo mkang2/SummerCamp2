dd1fb712b5a597621a41fb3ece20a8b4f207c58e
Robert Kang <65458638+rokanq@users.noreply.github.com>
2024-05-23 16:28:37 -0400

    Tailwind css overhaul

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header';
+import Footer from './footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './home';
+import AboutPage from './about';
+
+function App() {
+  return (
+    <Router>
+      <div className='flex flex-col min-h-screen'>
+        <Header />
+        <div className="flex-grow">
+          {/* Setup the Router Switch and Routes here */}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>        
+        </div>
+        <Footer />
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <img src="\Frame3.png" alt="summer camp photo"></img>
+      <img src="\frame-4@2x.png" alt="kid painting"></img>
+
+    </div>
+  );
+}
+
+export default HomePage;
