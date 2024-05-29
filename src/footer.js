7d43c75ee6d52cf07cd0eb3556526f7bee62d78d
mkang2 <kangmichaelj1@gmail.com>
2024-05-29 14:24:04 -0400

    home page content, colors

--- src/footer.js
+++ src/footer.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React from "react";
 //import { PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/outline';
-import { PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/20/solid';
+import { PhoneIcon, EnvelopeIcon, MapPinIcon } from "@heroicons/react/20/solid";
 function Footer() {
-    return (
-        <footer className="bg-yellow-100 py-6 px-10">
-            <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-4 items-center">
-                <div className="md:pl-24 space-y-2">
-                    <div className="flex items-center space-x-4">
-                        <img src="/Logo.png" alt="Calvary Logo" className="h-8 w-auto" />
-                        <span className="font-bold text-xl">Calvary Summer Camp</span>
-                    </div>
-                    <p className="text-gray-600 text-sm md:pl-12">
-                        © 2024 Calvary KUMC. All rights reserved
-                    </p>
-                </div>
-                <div className="text-gray-700 text-sm space-y-1 md:pt-8">
-                    <div className='text-lg font-bold'>Contact Us</div>
-                    <div className='flex'><PhoneIcon className='text-black w-5 h-5 mr-2' /> (917) 282-5706; (908) 930-6483</div>
-                    <div className='flex'><EnvelopeIcon className='text-black w-5 h-5 mr-2' /> calvarykumc.summercamp@gmail.com</div>
-                    <div className='flex'> <MapPinIcon className='text-black w-5 h-5 mr-2' />572 Ryders Lane, East Brunswick, NJ 08816</div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-yellow-50 py-6 px-10">
+      <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-4 items-center">
+        <div className="md:pl-24 space-y-2">
+          <div className="flex items-center space-x-4">
+            <img src="/Logo.png" alt="Calvary Logo" className="h-8 w-auto" />
+            <span className="font-bold text-xl">Calvary Summer Camp</span>
+          </div>
+          <p className="text-gray-600 text-sm md:pl-12">
+            © 2024 Calvary KUMC. All rights reserved
+          </p>
+        </div>
+        <div className="text-gray-700 text-sm space-y-1 md:pt-8">
+          <div className="text-lg font-bold">Contact Us</div>
+          <div className="flex">
+            <PhoneIcon className="text-black w-5 h-5 mr-2" /> (917) 282-5706;
+            (908) 930-6483
+          </div>
+          <div className="flex">
+            <EnvelopeIcon className="text-black w-5 h-5 mr-2" />{" "}
+            calvarykumc.summercamp@gmail.com
+          </div>
+          <div className="flex">
+            {" "}
+            <MapPinIcon className="text-black w-5 h-5 mr-2" />
+            572 Ryders Lane, East Brunswick, NJ 08816
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/header.js
+++ src/header.js
@@ -1,85 +1,155 @@
-import { useState } from 'react';
-import { Dialog, Popover, Button } from '@headlessui/react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Dialog, Popover, Button } from "@headlessui/react";
+import { Link } from "react-router-dom";
 import {
-    ArrowPathIcon,
-    Bars3Icon,
-    ChartPieIcon,
-    CursorArrowRaysIcon,
-    FingerPrintIcon,
-    SquaresPlusIcon,
-    XMarkIcon,
-} from '@heroicons/react/24/outline';
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
 
 export default function Example() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    return (
-        <header className="bg-yellow-100">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-                <div className="flex lg:flex-1">
-                    <Link to="/" className="flex items-center space-x-4">
-                        <img className="h-8 w-auto" src="/Logo.png" alt="Calvary Logo" />
-                        <span className="font-bold text-xl">Calvary Summer Camp</span>
-                    </Link>
-                </div>
-                <div className="flex lg:hidden">
-                    <button
-                        type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                        onClick={() => setMobileMenuOpen(true)}
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                </div>
-                <Popover.Group className="hidden lg:flex lg:gap-x-16">
-                    <Link to="/" className="text-md font-semibold leading-6 text-gray-900">Home</Link>
-                    <Link to="/about" className="text-md font-semibold leading-6 text-gray-900">About</Link>
-                    <Link to="/program" className="text-md font-semibold leading-6 text-gray-900">Program</Link>
-                    <Link to="/volunteer" className="text-md font-semibold leading-6 text-gray-900">Volunteer</Link>
-                    <Link to="/contact" className="text-md font-semibold leading-6 text-gray-900">Contact</Link>
-                </Popover.Group>
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <Link to="/register" className="rounded-2xl bg-yellow-400 py-2 px-4 text-sm text-white font-semibold hover:bg-yellow-300">
-                        Register
-                    </Link>
-                </div>
-            </nav>
-            <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 z-10" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-yellow-100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                    <div className="flex items-center justify-between">
-                        <Link to="/" className="flex items-center space-x-4">
-                            <img className="h-8 w-auto" src="/Logo.png" alt="Calvary Logo" />
-                            <span className="font-bold text-xl">Calvary Summer Camp</span>
-                        </Link>
-                        <button
-                            type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                            onClick={() => setMobileMenuOpen(false)}
-                        >
-                            <span className="sr-only">Close menu</span>
-                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                        </button>
-                    </div>
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="space-y-2 py-6">
-                                <Link to="/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</Link>
-                                <Link to="/about" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</Link>
-                                <Link to="/program" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Program</Link>
-                                <Link to="/volunteer" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Volunteer</Link>
-                                <Link to="/contact" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contact</Link>
-                            </div>
-                            <div className="py-6">
-                                <Link to="/register" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Register</Link>
-                            </div>
-                        </div>
-                    </div>
-                </Dialog.Panel>
-            </Dialog>
-        </header>
-    );
+  return (
+    <header className="bg-yellow-50">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link to="/" className="flex items-center space-x-4">
+            <img className="h-8 w-auto" src="/Logo.png" alt="Calvary Logo" />
+            <span className="font-bold text-xl">Calvary Summer Camp</span>
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-16">
+          <Link
+            to="/"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
+            About
+          </Link>
+          <Link
+            to="/program"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
+            Program
+          </Link>
+          <Link
+            to="/volunteer"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
+            Volunteer
+          </Link>
+          <Link
+            to="/contact"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
+            Contact
+          </Link>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <Link
+            to="https://docs.google.com/forms/d/e/1FAIpQLSfLalY5hj0OM9tmiKdXrSOY0ltDpwPwAGk6dgxKQX1ka7_uzA/viewform"
+            target="_blank"
+            className="rounded-2xl bg-yellow-400 py-2 px-4 text-sm text-white font-semibold hover:bg-yellow-300"
+          >
+            Register
+          </Link>
+        </div>
+      </nav>
+      <Dialog
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-yellow-50 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <Link to="/" className="flex items-center space-x-4">
+              <img className="h-8 w-auto" src="/Logo.png" alt="Calvary Logo" />
+              <span className="font-bold text-xl">Calvary Summer Camp</span>
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Link
+                  to="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  About
+                </Link>
+                <Link
+                  to="/program"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Program
+                </Link>
+                <Link
+                  to="/volunteer"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Volunteer
+                </Link>
+                <Link
+                  to="/contact"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Contact
+                </Link>
+              </div>
+              <div className="py-6">
+                <Link
+                  to="/register"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Register
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
 }
